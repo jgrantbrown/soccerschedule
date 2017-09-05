50112d34055f111527b2a569a99c25b53f5eed28
JGBrown <j.grant.brown@gmail.com>
2017-09-05 12:23:40 -0400

    add specmd file

--- lib/soccer_schedule/cli.rb
+++ lib/soccer_schedule/cli.rb
@@ -1,10 +1,13 @@
 class SoccerSchedule::CLI
 
+
   def call
-    puts "Welcome to Soccer Schedules"
+  puts "Welcome to Soccer Schedules"
+  puts " "
   puts '  o__        o__     |   |\ '
   puts ' /|          /\      |   |X\ '
   puts ' / > o        <\     |   |XX\ '
+  puts ''
   puts "Which team schedule do you want?"
     list_teams
     menu
@@ -17,7 +20,7 @@ end
 def menu
   input=nil
   while input != "exit"
-    puts "PLease select a team by number, 'list' for teams, or 'exit'"
+    puts "Please select a team by number, 'list' for teams, or 'exit'"
     input= gets.strip
     case input
     when "1"
--- lib/soccer_schedule/teams.rb
+++ lib/soccer_schedule/teams.rb
@@ -1,8 +1,21 @@
 class SoccerSchedule::Teams
-
+ attr_accessor :name, :url
 def self.all
+  teams=[]
+  # div banner-club-container
+  # attr("href")
+  # banner-logo title= team-name
+  doc = Nokogiri::HTML("https://www.mlssoccer.com/")
+  doc.search("div banner-club-container")
+
   puts "1. Redbulls"
   puts "2. NYCFC"
   puts "3. DC United"
+
+  # teams<<
 end
+
+
+
+
 end
