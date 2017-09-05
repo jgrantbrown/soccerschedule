7048138fe4ce138886031a1c0bc5f8180f929c6e
JGBrown <j.grant.brown@gmail.com>
2017-09-05 13:08:10 -0400

    scraping forn team names

--- lib/soccer_schedule.rb
+++ lib/soccer_schedule.rb
@@ -1,3 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+# remove for puplishing
+require 'pry'
+
 require_relative "./soccer_schedule/version"
 require_relative './soccer_schedule/cli'
 require_relative './soccer_schedule/teams'
--- lib/soccer_schedule/teams.rb
+++ lib/soccer_schedule/teams.rb
@@ -1,16 +1,25 @@
 class SoccerSchedule::Teams
  attr_accessor :name, :url
+
+
 def self.all
   teams=[]
+  teams_url=[]
   # div banner-club-container
   # attr("href")
   # banner-logo title= team-name
-  doc = Nokogiri::HTML("https://www.mlssoccer.com/")
-  doc.search("div banner-club-container")
+  doc = Nokogiri::HTML(open("https://www.mlssoccer.com/"))
 
-  puts "1. Redbulls"
-  puts "2. NYCFC"
-  puts "3. DC United"
+  doc.css("a.banner-club-logo").map do |each|
+    teams<<each.attr("title")
+    teams_url<< each.attr("href")
+  end
+  # team_name = doc.search("a.banner-club-logo").first.attr("title")
+  # team_url = doc.search("a.banner-club-logo").first.attr("href")
+  # puts "1. Redbulls"
+  # puts "2. NYCFC"
+  # puts "3. DC United"
+  puts teams
 
   # teams<<
 end
