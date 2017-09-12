0ff653202eaea3a5fc891837da5204f8275065e7
JGBrown <j.grant.brown@gmail.com>
2017-09-12 13:41:56 -0400

    fix exit goodbye method

--- lib/soccer_schedule/cli.rb
+++ lib/soccer_schedule/cli.rb
@@ -10,6 +10,7 @@ class SoccerSchedule::CLI
     puts "Which team schedule do you want?"
       list_teams
       menu
+      goodbye
   end
 
   def list_teams
@@ -22,23 +23,23 @@ class SoccerSchedule::CLI
   def menu
     input=nil
     @teams = SoccerSchedule::Teams.all
-
-    while input != "exit"
-      puts "Please select a team by number, 'list' for teams, or 'exit'"
-      input = gets.strip
-
-      
-      if (1...@teams.length).include?(input.to_i)
-          puts "#{@teams[input.to_i - 1][:name]} next game info:"
-
-        @schedule= SoccerSchedule::Teams.team_schedule(input)
-        menu
-      elsif input == "list"
-         list_teams
-      else
-        puts "Not valid entry. Type list or exit"
+    @schedule= SoccerSchedule::Teams.team_schedule(input)
+      while input != "exit"
+        puts "Please select a team by number, 'list' for teams, or 'exit'"
+        input = gets.strip.downcase
+        if (1...@teams.length).include?(input.to_i)
+            puts "#{@teams[input.to_i - 1][:name]} next game info:"
+            SoccerSchedule::Teams.team_schedule(input)
+          menu
+        elsif input == "list"
+           list_teams
+        else
+          puts "Not valid entry. Type list or exit"
+        end
       end
-    end
   end
 
+  def goodbye
+    puts "Have fun watching the game!"
+  end
 end
