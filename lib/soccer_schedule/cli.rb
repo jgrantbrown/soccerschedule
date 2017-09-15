d9a433a66fcba8145c412195309a6f5561e8ad3c
JGBrown <j.grant.brown@gmail.com>
2017-09-15 09:24:04 -0400

    refactored for initilaize of teams

--- lib/soccer_schedule/cli.rb
+++ lib/soccer_schedule/cli.rb
@@ -13,9 +13,12 @@ class SoccerSchedule::CLI
   end
 
   def list_teams
+
+    SoccerSchedule::Teams.scrape_teams
+
     @teams = SoccerSchedule::Teams.all
     @teams.each.with_index(1)  do |teams,i|
-       puts "#{i}. #{teams[:name]}"
+       puts "#{i}. #{teams.name}"
     end
   end
 
@@ -27,12 +30,13 @@ class SoccerSchedule::CLI
         puts "Please select a team by number, 'list' for teams, or 'exit'"
         input = gets.strip.downcase
         if (1...@teams.length).include?(input.to_i)
-            puts "#{@teams[input.to_i - 1][:name]}  info:"
+            
+            puts "#{@teams[input.to_i - 1].name}  info:"
             # puts "Enter 'Next Game' , 'Roster', 'List Teams' or 'Exit'"
             # input= get.strip.downcase
             #run method to check input and run needed method to dipslay correct info
             SoccerSchedule::Teams.team_schedule(input)
-            
+
         elsif input == "list"
            list_teams
         else
--- lib/soccer_schedule/teams.rb
+++ lib/soccer_schedule/teams.rb
@@ -3,84 +3,69 @@ class SoccerSchedule::Teams
 
 
 # REFACTOR FOR BETTER PRACTICE
- # @@all=[]
- #
- #  def initialize(name, url)
- #    @name= name
- #    @url = url
- #    @schedule_url = url
- #    @next_game = next_game
- #    @@all<<self
- #  end
- #
- #  def self.all
- #    @@all
- #  end
- #
- # def self.find(id)
- #   self.all[id-1]
- # end
+ @@all=[]
 
-#  def self.new_from_home(team)
-#     self.new(
-#       name,
-#       url,
-#       schedule_url
-#       next_game)
-# end
-
-
+  def initialize(name=nil, url=nil, schedule_url=nil)
+    @name= name
+    @url = url
+    @schedule_url = url
+    @next_game = next_game
+    @@all<<self
+  end
 
   def self.all
-    teams=[]
-    doc = Nokogiri::HTML(open("https://www.mlssoccer.com/"))
-    doc.css("a.banner-club-logo").map do |team|
-      name = team.attr("title")
-      url=""
-
-      # hard coded descrepency in url form mls page scrape and actual web address
-      if  team.attr("href")== "http://www.portlandtimbers.com/"
-        url = "http://www.timbers.com/"
-      elsif team.attr("href")== "http://www.realsaltlake.com/"
-        url = "http://www.rsl.com/"
-      else
-        url=team.attr("href")
-      end
-      # Option #1
-      uri = URI.parse(url)
-      uri.scheme = "https"
-      uri.to_s
-      #option #2
-      # url.gsub('/http/','https')
-      teams<<{:name => name,:url => url, :schedule_url => "#{uri.to_s}schedule"}
-    end
-
-    teams
+    @@all
   end
 
 
-   def self.team_schedule(input)
+  def self.team_schedule(input)
     @teams= self.all
     @team= @teams[input.to_i-1]
-    if @team[:name] == "Los Angeles FC"
-      @team[:next_game]= "No schedule yet"
+    if @team.name == "Los Angeles FC"
+      @team.next_game= "No schedule yet"
     else
-         doc = Nokogiri::HTML(open("#{@team[:schedule_url]}", :allow_redirections => :safe))
+         doc = Nokogiri::HTML(open("#{@team.schedule_url}", :allow_redirections => :safe))
          date = doc.css("div.match_details.upcoming").text
          time = doc.css("div.match_header div.match_info").text
          team_1 = doc.css("li.last div.match_container div.club_container img.club_logo").attr("title").value
          team_2 = doc.css("li.last div.match_container div.club_container.clubtwo img.club_logo").attr("title").value
          location = doc.css("li.last div.match_footer div.match_info").text
-         @team[:next_game]={ :date=>date,:time=>time,:team_1=>team_1, :team_2=>team_2,:location=>location}
+         @team.next_game={ :date=>date,:time=>time,:team_1=>team_1, :team_2=>team_2,:location=>location}
          puts "**********************************************************************"
-         puts "*** #{@team[:next_game][:team_1]} vs #{@team[:next_game][:team_2]}"
-         puts "***    #{@team[:next_game][:date]} @ #{@team[:next_game][:time]}  "
-         puts "***    Location: #{@team[:next_game][:location]}                  "
+         puts "*** #{@team.next_game[:team_1]} vs #{@team.next_game[:team_2]}"
+         puts "***    #{@team.next_game[:date]} @ #{@team.next_game[:time]}  "
+         puts "***    Location: #{@team.next_game[:location]}                  "
          puts "**********************************************************************"
          @team
        end
    end
 
+   def self.scrape_teams
+
+     doc = Nokogiri::HTML(open("https://www.mlssoccer.com/"))
+     doc.css("a.banner-club-logo").map do |team|
+       name = team.attr("title")
+       url=""
+
+       # hard coded descrepency in url form mls page scrape and actual web address
+       if  team.attr("href")== "http://www.portlandtimbers.com/"
+         url = "http://www.timbers.com/"
+       elsif team.attr("href")== "http://www.realsaltlake.com/"
+         url = "http://www.rsl.com/"
+       else
+         url=team.attr("href")
+       end
+       # Option #1
+       uri = URI.parse(url)
+       uri.scheme = "https"
+       uri.to_s
+       #option #2
+       # url.gsub('/http/','https')
+       SoccerSchedule::Teams.new( name,url,  "#{uri.to_s}schedule")
+     end
+
+
+   end
 
 
 end
