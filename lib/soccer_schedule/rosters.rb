b9658c9f4c7b27fcf61094c599235aa63b9054a2
JGBrown <j.grant.brown@gmail.com>
2017-09-26 13:06:43 -0400

    small refactor of code teams

--- lib/soccer_schedule/rosters.rb
+++ lib/soccer_schedule/rosters.rb
@@ -2,42 +2,42 @@ class SoccerSchedule::Rosters
   attr_accessor :team, :players
 
   # REFACTOR FOR BETTER PRACTICE
-
-
-   def initialize(players=nil)
+  def initialize(players=nil)
      @players = []
+  end
 
-   end
 # add ability to scrape for team rosters to be added to team object
   def self.team_roster(input)
-     @teams= SoccerSchedule::Teams.all
-     @team= @teams[input.to_i-1]
 
-           if @team.name == "Los Angeles FC"
-             @team.roster= "No team info yet"
-           else
-              players=[]
-              doc = Nokogiri::HTML(open("#{@team.roster_url}", :allow_redirections => :safe))
+     @team = self.find_team(input)
 
-              doc.css("ul.player_list li.row div.player_info").map do |plyr_name|
-                  fullname = plyr_name.css("div.name a.name_link").text
-                  position = plyr_name.css("span.position").text
-                    # THis is grouping all names?
-                  add_player = SoccerSchedule::Players.new(fullname,position)
-                  add_player.team=@team.name
-                  players<<add_player
-                  @team.roster=players
-                end
-            end
-    end
+     if @team.name == "Los Angeles FC"
+          @team.roster= "No team info yet"
+     else
+          doc = Nokogiri::HTML(open("#{@team.roster_url}", :allow_redirections => :safe))
 
-def self.players(input)
-  SoccerSchedule::Rosters.team_roster(input)
-  @teams= SoccerSchedule::Teams.all
-  @team= @teams[input.to_i-1]
-   @team.roster.each.with_index(1) do |player,i|
-     puts "#{i}. #{player.name}  POSITION: #{player.position}"
+          doc.css("ul.player_list li.row div.player_info").map do |plyr_name|
+            fullname = plyr_name.css("div.name a.name_link").text
+            position = plyr_name.css("span.position").text
+            # THis is grouping all names?
+            add_player = SoccerSchedule::Players.new(fullname,position)
+            add_player.team=@team.name
+            @team.roster ||= []
+            @team.roster<<add_player
+          end
+      end
   end
-end
+
+  def self.players(input)
+    SoccerSchedule::Rosters.team_roster(input)
+    @team = self.find_team(input)
+    @team.roster.each.with_index(1) do |player,i|
+       puts "#{i}. #{player.name}  POSITION: #{player.position}"
+    end
+  end
+
+  def self.find_team(input)
+    SoccerSchedule::Teams.all[input.to_i-1]
+  end
 
 end
--- lib/soccer_schedule/teams.rb
+++ lib/soccer_schedule/teams.rb
@@ -1,7 +1,6 @@
 class SoccerSchedule::Teams
  attr_accessor :name, :url, :schedule_url, :next_game, :roster, :roster_url
-
-# REFACTOR FOR BETTER PRACTICE
+  # REFACTOR FOR BETTER PRACTICE
  @@all=[]
 
   def initialize(name=nil, url=nil, schedule_url=nil, roster_url=nil)
@@ -18,56 +17,61 @@ class SoccerSchedule::Teams
     @@all
   end
 
-
   def self.team_schedule(input)
-    @teams= self.all
-    @team= @teams[input.to_i-1]
-    if @team.name == "Los Angeles FC"
-      @team.next_game= "No schedule yet"
-    else
-         doc = Nokogiri::HTML(open("#{@team.schedule_url}", :allow_redirections => :safe))
-         date = doc.css("div.match_details.upcoming").text
-         time = doc.css("div.match_header div.match_info").text
-         team_1 = doc.css("li.last div.match_container div.club_container img.club_logo").attr("title").value
-         team_2 = doc.css("li.last div.match_container div.club_container.clubtwo img.club_logo").attr("title").value
-         location = doc.css("li.last div.match_footer div.match_info").text
-         @team.next_game={ :date=>date,:time=>time,:team_1=>team_1, :team_2=>team_2,:location=>location}
-         puts "**********************************************************************"
-         puts "*** #{@team.next_game[:team_1]} vs #{@team.next_game[:team_2]}"
-         puts "***    #{@team.next_game[:date]} @ #{@team.next_game[:time]}  "
-         puts "***    Location: #{@team.next_game[:location]}                  "
-         puts "**********************************************************************"
-         @team
-       end
+
+    @team = self.find_team(input.to_i)
+
+      if @team.name == "Los Angeles FC"
+        @team.next_game= "No schedule yet"
+      else
+           doc = Nokogiri::HTML(open("#{@team.schedule_url}", :allow_redirections => :safe))
+           date = doc.css("div.match_details.upcoming").text
+           time = doc.css("div.match_header div.match_info").text
+           team_1 = doc.css("li.last div.match_container div.club_container img.club_logo").attr("title").value
+           team_2 = doc.css("li.last div.match_container div.club_container.clubtwo img.club_logo").attr("title").value
+           location = doc.css("li.last div.match_footer div.match_info").text
+           @team.next_game={ :date=>date,:time=>time,:team_1=>team_1, :team_2=>team_2,:location=>location}
+
+           @team.game_info
+      end
    end
 
    def self.scrape_teams
 
      doc = Nokogiri::HTML(open("https://www.mlssoccer.com/"))
+
      doc.css("a.banner-club-logo").map do |team|
-
        name = team.attr("title")
-       url=""
-
-       # hard coded descrepency in url form mls page scrape and actual web address
-       if  team.attr("href")== "http://www.portlandtimbers.com/"
-         url = "http://www.timbers.com/"
-       elsif team.attr("href")== "http://www.realsaltlake.com/"
-         url = "http://www.rsl.com/"
-       else
-         url=team.attr("href")
-       end
-       # Option #1
-       uri = URI.parse(url)
-       uri.scheme = "https"
-       uri.to_s
-       #option #2
-       # url.gsub('/http/','https')
-       SoccerSchedule::Teams.new( name,url, "#{uri.to_s}schedule","#{uri.to_s}players")
+       url = ""
+         # hard coded descrepency in url form mls page scrape and actual web address
+         if  team.attr("href")== "http://www.portlandtimbers.com/"
+           url = "http://www.timbers.com/"
+         elsif team.attr("href")== "http://www.realsaltlake.com/"
+           url = "http://www.rsl.com/"
+         else
+           url=team.attr("href")
+         end
+         # Option #1
+        uri = URI.parse(url)
+        uri.scheme = "https"
+        uri.to_s
+         #option #2
+         # url.gsub('/http/','https')
+        SoccerSchedule::Teams.new( name,url, "#{uri.to_s}schedule","#{uri.to_s}players")
      end
-
-
    end
+
+   def self.find_team(input)
+       self.all[input.to_i-1]
+   end
+
+   def game_info
+     puts "**********************************************************************"
+     puts "*** #{self.next_game[:team_1]} vs #{self.next_game[:team_2]}"
+     puts "***    #{self.next_game[:date]} @ #{self.next_game[:time]}  "
+     puts "***    Location: #{self.next_game[:location]}                  "
+     puts "**********************************************************************"
+  end
 
 
 end
